16463e786fa0e88a8506169b14adffa6940f36bd
BradL <34219856+bradlindblad@users.noreply.github.com>
2024-01-22 20:12:04 -0600

    testing netlify

---
+++ link_check.R
@@ -0,0 +1,81 @@
+
+#' Flatten copy
+#'
+#' @param from Source directory path.
+#' @param to Destination directory path.
+#'
+#' @return Destination directory path.
+#'
+#' @details
+#' Copy all `.Rmd`, `.qmd`, and `.md` files from source to destination,
+#' rename the `.qmd` and `.md` files with an additional `.Rmd` extension,
+#' and get a flat destination structure with path-preserving file names.
+flatten_copy <- function(from, to) {
+  from  = "."
+  to = "foo"
+  rmd <- list.files(from, pattern = "\\.Rmd$", recursive = TRUE, full.names = TRUE)
+  xmd <- list.files(from, pattern = "\\.qmd$|\\.md$", recursive = TRUE, full.names = TRUE)
+  
+  src <- c(rmd, xmd)
+  dst <- c(rmd, paste0(xmd, ".Rmd"))
+  
+  # Remove starting `./` (if any)
+  dst <- gsub("^\\./", replacement = "", x = dst)
+  # Replace the forward slash in path with Unicode big solidus
+  dst <- gsub("/", replacement = "\u29F8", x = dst)
+  
+  file.copy(src, to = file.path(to, dst))
+  
+  invisible(to)
+}
+
+
+
+#' Check URLs in an R Markdown or Quarto project
+#'
+#' @param input Path to the project directory.
+#'
+#' @return URL checking results from `urlchecker::url_check()`
+#' for all `.Rmd`, `.qmd`, and `.md` files in the project.
+#'
+#' @details
+#' The `tools::pkgVignettes()$docs` call in urlchecker requires
+#' two core criteria (`VignetteBuilder` and `VignetteEngine`)
+#' to recognize `.Rmd` files as package vignettes.
+check_url <- function(input = ".") {
+  # Create a source package directory
+  input="foo"
+  dir.create("foo")
+  flatten_copy(".", "foo")
+  # pkg <- "foo/bar"
+  dir.create("bar")
+  dir.create("bar/vignettes")
+  fyles <- fs::dir_ls("foo", recurse = TRUE)
+  fs::file_copy(fyles, "bar/vignettes")
+  
+  # Flatten copy relevant files
+  vig <- file.path(pkg, "vignettes")
+  dir.create(vig)
+  # copy_qmd_files(input, vig)
+  # flatten_copy(input, vig)
+  
+  # Create a minimal DESCRIPTION file
+  write("VignetteBuilder: knitr", file = file.path("bar", "DESCRIPTION"))
+  
+  # Make the copied files look like vignettes
+  lapply(
+    list.files("bar/vignettes"),
+    function(x) {
+      write(
+        "---\nvignette: >\n  %\\VignetteEngine{knitr::rmarkdown}\n---",
+        file = x, append = TRUE
+      )
+    }
+  )
+  # fs::dir_copy("foo", "foo/bar/vignettes")
+  
+  urlchecker::url_check("bar")
+}
+
+check_url()
+
